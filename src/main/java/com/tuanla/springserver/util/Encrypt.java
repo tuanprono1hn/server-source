09b37ad59333cdd4f1bf8cca9fc53046f6ec2b0f
tuanla@telsoft.com.vn <tuanla@telsoft.com.vn>
2024-10-30 00:05:54 +0700

    add Encryption and Decryption

---
+++ src/main/java/com/tuanla/springserver/util/Encrypt.java
@@ -0,0 +1,23 @@
+package com.tuanla.springserver.util;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class Encrypt {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
+        String input = "baeldung";
+        SecretKey key = AESUtil.generateKey(128);
+        IvParameterSpec ivParameterSpec = AESUtil.generateIv();
+        String algorithm = "AES/CBC/PKCS5Padding";
+        String cipherText = AESUtil.encrypt(algorithm, input, key, ivParameterSpec);
+        String plainText = AESUtil.decrypt(algorithm, cipherText, key, ivParameterSpec);
+        System.out.println(cipherText);
+        System.out.println(plainText);
+    }
+}
